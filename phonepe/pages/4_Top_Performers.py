96bf2936ff028ff507c92f22ee43d7c60ca884b0
Nirmalkumar <nk4544485@gmail.com>
2026-03-28 01:22:59 +0530

    Delete phonepe directory

--- phonepe/pages/4_Top_Performers.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Top Performers | PhonePe Pulse", page_icon="🏆")
-st.title("🏆 Top Performers")
-st.write("Displays leading states/districts by metrics.")
